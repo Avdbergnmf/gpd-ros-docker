38f688dd049659e2c715ba255fbbadb36f75218f
Alex <alex_van_den_berg@hotmail.com>
2021-05-06 18:00:26 +0200

    Start script for PAL container

---
+++ start_container_PAL.sh
@@ -0,0 +1,17 @@
+# allow x server connection
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+xhost +local:root
+# to set up the right environment variables in CLion
+echo "Set \$DISPLAY parameter to $DISPLAY" 
+
+CONTAINER_NAME=pal_tiago
+
+docker start $CONTAINER_NAME
+docker exec -it $CONTAINER_NAME terminator -u
+# docker attach $CONTAINER_NAME
+
+# disallow x server connection
+xhost -local:root
